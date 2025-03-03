b1b9f61a2e38eb5e2f931813120ddd0598486549
EvRy111001110001 <ekaterina.post91@gmail.com>
2025-03-03 20:42:42 +0500

    second commit

---
+++ src/main/java/com/evry/studycomputerassembler/StudyComputerAssemblerApplication.java
@@ -0,0 +1,13 @@
+package com.evry.studycomputerassembler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyComputerAssemblerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyComputerAssemblerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evry/studycomputerassembler/StudyComputerAssemblerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evry.studycomputerassembler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyComputerAssemblerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
